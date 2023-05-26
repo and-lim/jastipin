a80b03a0c8a629deda1c04dbccd18e6b879fb298
and-lim <andrianlimnardy@gmail.com>
2023-05-26 18:37:48 +0700

    wrap cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,168 +6,189 @@
     <div class="container mt-3 py-5">
         <h1 class="title mt-3 pb-5">Cart</h1>
 
-        <div class="card col-lg-10 mx-auto shadow-sm p-3">
+        <div class="col-lg-12">
             @if ($errors->any())
             <div class="alert alert-dark" role="alert" style="outline: none">
                 <i class="text-danger mt-1">{{$errors->first()}}</i>
             </div>
             @endif
+            <div class="card p-3">
+                @php
 
-            @php
+                $sumItemPriceTrip = array();
+                $sumRequestPriceTrip = array();
+                $sumItemAmount = array();
+                $sumRequestAmount = array();
+                $sumBeacukaiPabean = array();
+                $item_array = array();
+                $request_array = array();
+                $array_cart_trip = array();
 
-            $sumItemPriceTrip = array();
-            $sumRequestPriceTrip = array();
-            $sumItemAmount = array();
-            $sumRequestAmount = array();
-            $sumBeacukaiPabean = array();
-            $item_array = array();
-            $request_array = array();
-            $array_cart_trip = array();
+                $sumItemPrice = 0;
+                $sumItemQuantity = 0;
+                $sumItemWeight = 0;
+                $sumItemPpn = 0;
+                $sumItemPabean = 0;
 
-            $sumItemPrice = 0;
-            $sumItemQuantity = 0;
-            $sumItemWeight = 0;
-            $sumItemPpn = 0;
-            $sumItemPabean = 0;
+                $sumRequestPrice = 0;
+                $sumRequestQuantity = 0;
+                $sumRequestWeight = 0;
+                $sumRequestPpn = 0;
+                $sumRequestPabean = 0;
 
-            $sumRequestPrice = 0;
-            $sumRequestQuantity = 0;
-            $sumRequestWeight = 0;
-            $sumRequestPpn = 0;
-            $sumRequestPabean = 0;
+                @endphp
 
-            @endphp
-
-            @foreach ($cart_trip as $trip)
-            @php
-            $contain_item_price = 0;
-            $contain_request_price = 0;
-            $contain_item_quantity = 0;
-            $contain_request_quantity = 0;
-            $contain_beacukai_pabean = 0;
-            $array_cart_trip[$trip->trip_id] = $trip;
-            @endphp
-
-
-            <p>{{ $trip->destination }} - {{ $trip->origin }}</p>
-
-            @foreach($array_trip[$trip->trip_id]->items as $item)
+                @foreach ($cart_trip as $trip)
+                @php
+                $contain_item_price = 0;
+                $contain_request_price = 0;
+                $contain_item_quantity = 0;
+                $contain_request_quantity = 0;
+                $contain_beacukai_pabean = 0;
+                $array_cart_trip[$trip->trip_id] = $trip;
+                @endphp
 
 
-            @php
-            $sumItemQuantity = $sumItemQuantity + $item->cart_item_quantity;
-            $sumItemWeight = $sumItemWeight + $item->item_weight;
-            $sumItemPpn = $sumItemPpn + $item->item_price_ppn;
-            $sumItemPabean = $sumItemPabean + $item->item_price_pabean;
-            $sumItemPrice = $sumItemPrice + ($item->item_display_price * $item->cart_item_quantity) + $sumItemPpn + $sumItemPabean;
-            $contain_item_price = $contain_item_price + ($item->item_display_price * $item->cart_item_quantity) + $sumItemPpn + $sumItemPabean;
-            $contain_item_quantity = $contain_item_quantity + $item->cart_item_quantity;
-            $contain_beacukai_pabean = $contain_beacukai_pabean + $sumItemPpn + $sumItemPabean;
-            @endphp
 
 
-            <div class="item-cart border-bottom border-2 border-dark my-1">
-                <div class="row gap-3">
-                    <div class="col-lg-3">
-                        <img src="{{ asset('/storage/' .$item->item_image) }}" class="img-fluid" alt="">
+                @foreach($array_trip[$trip->trip_id]->items as $item)
+
+
+                @php
+                $sumItemQuantity = $sumItemQuantity + $item->cart_item_quantity;
+                $sumItemWeight = $sumItemWeight + $item->item_weight;
+                $sumItemPpn = $sumItemPpn + $item->item_price_ppn;
+                $sumItemPabean = $sumItemPabean + $item->item_price_pabean;
+                $sumItemPrice = $sumItemPrice + ($item->item_display_price * $item->cart_item_quantity) + $sumItemPpn + $sumItemPabean;
+                $contain_item_price = $contain_item_price + ($item->item_display_price * $item->cart_item_quantity) + $sumItemPpn + $sumItemPabean;
+                $contain_item_quantity = $contain_item_quantity + $item->cart_item_quantity;
+                $contain_beacukai_pabean = $contain_beacukai_pabean + $sumItemPpn + $sumItemPabean;
+                @endphp
+
+        
+                    <div class="item-cart card p-3 border-bottom border-2 border-dark my-1">
+                        <p class="text-start fw-bold text-primary">{{ $trip->destination }} - {{ $trip->origin }}</p>
+                        <div class="row gap-3">
+                            <div class="col-lg-3">
+                                <img src="{{ asset('/storage/' .$item->item_image) }}" class="img-fluid" alt="">
+                            </div>
+                            <div class="col-lg-8">
+                                <div class="item-desc pb-2">
+                                    <a href="/trip-detail/{{ $item->trip_id }}" class="text-decoration-none text-dark">
+                                        <h1 class="mb-2">{{ $item->item_name }}</h1>
+                                    </a>
+                                    <p>{{ $item->item_description }}</p>
+                                </div>
+                                <div class="row my-2 align-items-center">
+                                    <div class="col-lg-6">
+                                        <p>Rp {{ $item->item_display_price }}</p>
+                                        <p>{{ $item->item_weight }} Kg</p>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <p>PPN : Rp{{ $item->item_price_ppn }}</p>
+                                        <p>Pabean : Rp{{ $item->item_price_pabean }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-flex justify-content-end gap-2 mb-3">
+                            <input type="number" value="{{ $item->cart_item_quantity }}" min="1" max="{{ $item->item_stock }}" style="width: 50px">
+                            <form action="/deleteItemCart" method="POST">
+                                @csrf
+                                <input type="hidden" name="item_id" value="{{ $item->item_id }}">
+                                <button type="submit" class="btn btn-danger">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                        </div>
+
                     </div>
-                    <div class="col-lg-8">
-                        <a href="/trip-detail/{{ $item->trip_id }}" class="text-decoration-none text-dark">
-                            <h1 class="mb-2">{{ $item->item_name }}</h1>
-                        </a>
-                        <p>Rp {{ $item->item_display_price }}</p>
-                        <p>{{ $item->item_weight }} Kg</p>
-                        <p>{{ $item->item_description }}</p>
-                        <p>PPN : Rp{{ $item->item_price_ppn }}</p>
-                        <p>Pabean : Rp{{ $item->item_price_pabean }}</p>
+                    @endforeach
+
+                @foreach($array_trip[$trip->trip_id]->request_items as $request)
+
+                @php
+                $sumRequestQuantity = $sumRequestQuantity + $request->request_quantity;
+                $sumRequestWeight = $sumRequestWeight + $request->request_weight;
+                $sumRequestPpn = $sumRequestPpn + $request->request_price_ppn;
+                $sumRequestPabean = $sumRequestPabean + $request->request_price_pabean;
+                $sumRequestPrice = $sumRequestPrice + ($request->request_price * $request->request_quantity) + $sumRequestPpn + $sumRequestPabean;
+                $contain_request_price = $contain_request_price + ($request->request_price * $request->request_quantity) + $sumRequestPpn + $sumRequestPabean;
+                $contain_request_quantity = $contain_request_quantity + $item->cart_item_quantity;
+                $contain_beacukai_pabean = $contain_beacukai_pabean + $sumRequestPpn + $sumRequestPabean;
+                @endphp
+
+                    <div class="item-cart border-bottom border-2 border-dark my-1">
+                        <div class="row gap-3 my-2">
+                            <h3 class="text-start">Request</h3>
+                            <div class="col-lg-3">
+                                <img src="{{ asset('/storage/' .$request->request_image) }}" class="img-fluid" alt="">
+                            </div>
+                            <div class="col-lg-8">
+                                <div class="request-item">
+                                    <a href="/trip-detail/{{ $request->trip_id }}" class="text-decoration-none text-dark">
+                                        <h1 class="mb-2">{{ $request->request_name }}</h1>
+                                    </a>
+                                    <p>{{ $request->request_description }}</p>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <p>Rp {{ $request->request_price }}</p>
+                                        <p>{{ $request->request_quantity }} PC/s</p>
+                                        <p>{{ $request->request_weight }} Kg</p>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <p>{{ $request->request_status }}</p>
+                                        <p>PPN : Rp {{ $request->request_price_ppn }}</p>
+                                        <p>Pabean : Rp {{ $request->request_price_pabean }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="float-end d-flex gap-2 mb-3">
+                            <form action="/deleteRequestCart" method="POST">
+                                @csrf
+                                <input type="number" value="{{ $request->request_quantity }}" style="width: 50px">
+                                <input type="hidden" name="request_id" value="{{ $request->request_id }}">
+                                <button type="submit" class="btn btn-danger">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                        </div>
+                            {{-- details --}}
+                            <div class="cart-details 1 mt-2 py-5">
+                                <h3 class="mb-3">Details</h3>
+                                <div class="d-flex justify-content-between">
+                                    <p>total pesanan:</p>
+                                    <p>{{ $sumItemQuantity + $sumRequestQuantity }}</p>
+                                </div>
+                                <div class="d-flex justify-content-between">
+                                    <p>total price:</p>
+                                    <p>{{ $sumItemPrice + $sumRequestPrice }}</p>
+                                </div>
+                            </div>
+
                     </div>
-                </div>
+        
+                @endforeach
 
-                <div class="float-end d-flex gap-2 mb-3">
-                    <input type="number" value="{{ $item->cart_item_quantity }}" min="1" max="{{ $item->item_stock }}" style="width: 50px">
-                    <form action="/deleteItemCart" method="POST">
-                        @csrf
-                        <input type="hidden" name="item_id" value="{{ $item->item_id }}">
-                        <button type="submit" class="btn btn-danger">
-                            <i class="fa fa-trash"></i>
-                        </button>
-                    </form>
-                </div>
+                @php
+                $sumItemPriceTrip[$trip->trip_id] = $contain_item_price;
+                $sumRequestPriceTrip[$trip->trip_id] = $contain_request_price;
+                $sumItemAmount[$trip->trip_id] = $contain_item_quantity;
+                $sumRequestAmount[$trip->trip_id] = $contain_request_quantity;
+                $sumBeacukaiPabean[$trip->trip_id] = $contain_beacukai_pabean;
+                @endphp
 
+                <p>Total Item Price in this Trip: Rp {{ $contain_item_price }}</p>
+                <p>Total Request Price in this Trip: Rp {{ $contain_request_price }}</p>
+
+                @endforeach
             </div>
-            @endforeach
 
-            @foreach($array_trip[$trip->trip_id]->request_items as $request)
-
-            @php
-            $sumRequestQuantity = $sumRequestQuantity + $request->request_quantity;
-            $sumRequestWeight = $sumRequestWeight + $request->request_weight;
-            $sumRequestPpn = $sumRequestPpn + $request->request_price_ppn;
-            $sumRequestPabean = $sumRequestPabean + $request->request_price_pabean;
-            $sumRequestPrice = $sumRequestPrice + ($request->request_price * $request->request_quantity) + $sumRequestPpn + $sumRequestPabean;
-            $contain_request_price = $contain_request_price + ($request->request_price * $request->request_quantity) + $sumRequestPpn + $sumRequestPabean;
-            $contain_request_quantity = $contain_request_quantity + $item->cart_item_quantity;
-            $contain_beacukai_pabean = $contain_beacukai_pabean + $sumRequestPpn + $sumRequestPabean;
-            @endphp
-
-            <div class="item-cart border-bottom border-2 border-dark my-1">
-                <div class="row gap-3">
-                    <div class="col-lg-2">
-                        <img src="{{ asset('/storage/' .$request->request_image) }}" class="img-fluid" alt="">
-                    </div>
-                    <div class="col-lg-8">
-                        <a href="/trip-detail/{{ $request->trip_id }}">
-                            <h1 class="mb-2">{{ $request->request_name }}</h1>
-                        </a>
-                        <p>Rp {{ $request->request_price }}</p>
-                        <p>{{ $request->request_quantity }} PC/s</p>
-                        <p>{{ $request->request_weight }} Kg</p>
-                        <p>{{ $request->request_description }}</p>
-                        <p>{{ $request->request_status }}</p>
-                        <p>PPN : Rp {{ $request->request_price_ppn }}</p>
-                        <p>Pabean : Rp {{ $request->request_price_pabean }}</p>
-                    </div>
-                </div>
-
-                <div class="float-end d-flex gap-2 mb-3">
-                    <form action="/deleteRequestCart" method="POST">
-                        @csrf
-                        <input type="number" value="{{ $request->request_quantity }}" style="width: 50px">
-                        <input type="hidden" name="request_id" value="{{ $request->request_id }}">
-                        <button type="submit" class="btn btn-danger">
-                            <i class="fa fa-trash"></i>
-                        </button>
-                    </form>
-                </div>
-
-            </div>
-            @endforeach
-
-            @php
-            $sumItemPriceTrip[$trip->trip_id] = $contain_item_price;
-            $sumRequestPriceTrip[$trip->trip_id] = $contain_request_price;
-            $sumItemAmount[$trip->trip_id] = $contain_item_quantity;
-            $sumRequestAmount[$trip->trip_id] = $contain_request_quantity;
-            $sumBeacukaiPabean[$trip->trip_id] = $contain_beacukai_pabean;
-            @endphp
-
-            <p>Total Item Price in this Trip: Rp {{ $contain_item_price }}</p>
-            <p>Total Request Price in this Trip: Rp {{ $contain_request_price }}</p>
-
-            @endforeach
-
-            {{-- details --}}
-            <div class="cart-details 1 mt-2 py-5">
-                <h3 class="mb-3">Details</h3>
-                <div class="d-flex justify-content-between">
-                    <p>total pesanan:</p>
-                    <p>{{ $sumItemQuantity + $sumRequestQuantity }}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <p>total price:</p>
-                    <p>{{ $sumItemPrice + $sumRequestPrice }}</p>
-                </div>
-            </div>
+       
 
             <div class="d-flex justify-content-center">
                 <input type="hidden" name="$sumItemQuantity" value="{{ $sumItemQuantity }}">
